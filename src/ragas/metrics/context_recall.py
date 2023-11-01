0816c5db887d6b225a25367b736feb74d31d983f
Austin Welch <austinmw@users.noreply.github.com>
2023-11-01 01:02:40 -0400

    Remove blank sentences

--- src/ragas/metrics/context_recall.py
+++ src/ragas/metrics/context_recall.py
@@ -29,19 +29,25 @@ from ragas.metrics.base import EvaluationMode, MetricWithLLM
 # )
 CONTEXT_RECALL_RA = HumanMessagePromptTemplate.from_template(
     """<instructions>
-Given a context, and an answer, identify each individual sentence in the answer, analyze each, and classify if the sentence can be attributed to the given context or not.
-Carefully think step by step and provide detailed reasoning reflecting the context provided before coming to conclusion. Follow the exact output format as shown in the example response. Notice that there should not be any blank lines in your response!
+Given a context, and an answer, first identify each individual sentence in the answer, then analyze each, and classify if the sentence can be attributed to the given context or not.
+Carefully think step by step and provide detailed reasoning reflecting the context provided before coming to conclusion. 
+Follow the exact output format as shown in the example response. NEVER output double newlines in the response!
 </instructions>
 
 Here are some examples with responses:
 
 <example_input>
-<context>Albert Einstein (14 March 1879 – 18 April 1955) was a German-born theoretical physicist,widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, he also made important contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century. His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been called "the world's most famous equation". He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory. His work is also known for its influence on the philosophy of science. In a 1999 poll of 130 leading physicists worldwide by the British journal Physics World, Einstein was ranked the greatest physicist of all time. His intellectual achievements and originality have made Einstein synonymous with genius.</context>
-<answer>Albert Einstein born in 14 March 1879 was  German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics. He published 4 papers in 1905.  Einstein moved to Switzerland in 1895 </answer>
+<context>
+Albert Einstein (14 March 1879 – 18 April 1955) was a German-born theoretical physicist,widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, he also made important contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century. His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been called "the world's most famous equation". He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory. His work is also known for its influence on the philosophy of science. In a 1999 poll of 130 leading physicists worldwide by the British journal Physics World, Einstein was ranked the greatest physicist of all time. His intellectual achievements and originality have made Einstein synonymous with genius.
+</context>
+
+<answer>
+Albert Einstein born in 14 March 1879 was  German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics. He published 4 papers in 1905.  Einstein moved to Switzerland in 1895
+</answer>
 </example_input>
 
 <example_response>
-classification:
+Line by line sentence classifications for the given answer (with no ):
 1. Albert Einstein born in 14 March 1879 was  German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. The date of birth of Einstein is mentioned clearly in the context. So [Attributed]
 2. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics. The exact sentence is present in the given context. So [Attributed]
 3. He published 4 papers in 1905. There is no mention about papers he wrote in given the context. So [Not Attributed]
@@ -49,12 +55,17 @@ classification:
 </example_response>
 
 <example_input>
-<context>William Shakespeare (26 April 1564 – 23 April 1616) was an English playwright, poet, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist. He is often called England's national poet and the "Bard of Avon". His extant works, including some collaborations, consist of about 39 plays, 154 sonnets, and two long narrative poems. His plays have been translated into every major living language and are performed more often than those of any other playwright. He was born in Stratford-upon-Avon and married Anne Hathaway, with whom he had three children.</context>
-<answer>William Shakespeare was born on 26 April 1564 in Stratford-upon-Avon. He wrote 39 plays and 154 sonnets. Shakespeare was known as the Bard of Avon. He had two children with Anne Hathaway.</answer>
+<context>
+William Shakespeare (26 April 1564 – 23 April 1616) was an English playwright, poet, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist. He is often called England's national poet and the "Bard of Avon". His extant works, including some collaborations, consist of about 39 plays, 154 sonnets, and two long narrative poems. His plays have been translated into every major living language and are performed more often than those of any other playwright. He was born in Stratford-upon-Avon and married Anne Hathaway, with whom he had three children.
+</context>
+
+<answer>
+William Shakespeare was born on 26 April 1564 in Stratford-upon-Avon. He wrote 39 plays and 154 sonnets. Shakespeare was known as the Bard of Avon. He had two children with Anne Hathaway.
+</answer>
 </example_input>
 
 <example_response>
-classification:
+Line by line sentence classifications for the given answer:
 1. William Shakespeare was born on 26 April 1564 in Stratford-upon-Avon. The date and place of birth are mentioned clearly in the context. So [Attributed]
 2. He wrote 39 plays and 154 sonnets. The exact number of plays and sonnets is present in the given context. So [Attributed]
 3. Shakespeare was known as the Bard of Avon. This title is specifically mentioned in the context. So [Attributed]
@@ -62,10 +73,18 @@ classification:
 </example_response>
 
 Here is the input and answer for you to classify:
-<context>{context}</context>
-<answer>{ground_truth}</answer>
 
-Assistant: classification:
+<context>
+{context}
+</context>
+
+<answer>
+{ground_truth}
+</answer>
+
+Remember it is absolutely critical that you do not output double newlines in the response!
+
+Assistant: Line by line sentence classifications for the given answer:
 """  # noqa: E501
 )
 
@@ -121,6 +140,7 @@ class ContextRecall(MetricWithLLM):
             scores = []
             for response in responses:
                 sentences = response[0].split("\n")
+                sentences = [s for s in sentences if s.strip()]
 
                 for sentence in sentences:
                     print(f"sentence: {sentence}")
