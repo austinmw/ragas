48da51d9e7222e40cac29640efd2400cbb5505d7
Austin Welch <austinmw@users.noreply.github.com>
2023-11-12 19:48:52 -0500

    change calculation

--- src/ragas/metrics/context_mrr.py
+++ src/ragas/metrics/context_mrr.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass, field
 import numpy as np
 from datasets import Dataset
 from sentence_transformers import CrossEncoder
+from InstructorEmbedding import INSTRUCTOR
+from sklearn.metrics.pairwise import cosine_similarity
 
 from ragas.metrics.base import EvaluationMode, MetricWithLLM
 
@@ -23,9 +25,9 @@ logger.addHandler(handler)
 @dataclass
 class ContextMeanReciprocalRank(MetricWithLLM):
     """
-    Uses a cross encoder to score semantic similarity between the 
-    ground truth context and each retrieved document, then calculates
-    the Mean Reciprocal Rank of the retrieved documents.
+    Uses a the max of a thresholded semantic similarity score between the
+    ground truth context and each retrieved document or ground truth answer,
+    then calculates the Mean Reciprocal Rank of the max score above a threshold.
 
     Attributes
     ----------
@@ -33,27 +35,26 @@ class ContextMeanReciprocalRank(MetricWithLLM):
     batch_size : int
         Batch size for openai completion.
     embeddings:
-        The cross-encoder model to be used.
-        Defaults cross-encoder/stsb-roberta-large
-        Other good options https://huggingface.co/spaces/mteb/leaderboard
+        The embedding model to be used.
+        Defaults hkunlp/instructor-xl
+
     threshold:
         The threshold used to map output to binary
-        Default 0.6
+        Default 0.9
     """
 
     name: str = "context_mean_reciprocal_rank"
     evaluation_mode: EvaluationMode = EvaluationMode.qgacs
     batch_size: int = 16
-    embeddings: str | None = None
-    threshold: float | None = 0.6
+    threshold: float | None = 0.9
+    instruction ="Represent the document for retrieval:"
+    compare: str = "answer"
 
     latest_logs: dict = field(default_factory=dict)
 
     def __post_init__(self: t.Self):
-        if self.embeddings is None:
-            self.cross_encoder = CrossEncoder("cross-encoder/stsb-roberta-large")
-        else:
-            self.cross_encoder = CrossEncoder(self.embeddings)
+
+        self.model = INSTRUCTOR('hkunlp/instructor-xl')
 
     def _score_batch(
         self: t.Self,
@@ -67,10 +68,6 @@ class ContextMeanReciprocalRank(MetricWithLLM):
         answers = dataset["answer"]
         ground_truth_context = dataset["ground_truth_context"]
 
-        if ground_truth_context is None:
-            raise ValueError(("ContextMeanReciprocalRank error: "
-                              "ground_truth_context is None")
-            )
 
         # Log each item added to latest_logs
         self._log_and_update('question', questions[0])
@@ -81,27 +78,46 @@ class ContextMeanReciprocalRank(MetricWithLLM):
         self._log_and_update('generated_answer', answers[0])
         self._log_and_update('model_kwargs', self.llm.llm.model_kwargs)
 
+        if self.compare == "ground_truth_context":
+
+            if ground_truth_context is None:
+                raise ValueError(("ContextMeanReciprocalRank error: "
+                                "ground_truth_context is None")
+                )
+
         retrieved_contexts = contexts[0]
 
-        inputs = [list(item) for item in list(zip(ground_truth_context * \
-            len(retrieved_contexts), retrieved_contexts))]
+        retrieved_contexts_with_instruction = [
+            [self.instruction, context] for context in retrieved_contexts
+        ]
 
-        # Log threshold
-        scores = self.cross_encoder.predict(
-            inputs, batch_size=self.batch_size, convert_to_numpy=True
-        )
+        compare_with_instruction = [[self.instruction, dataset[self.compare]]]
+
+        embeddings_a = self.model.encode(compare_with_instruction)
+        embeddings_b = self.model.encode(retrieved_contexts_with_instruction)
+        similarity_scores = cosine_similarity(embeddings_a, embeddings_b)[0]
+
         # Log scores
-        self._log_and_update('scores', scores.tolist())
-        assert isinstance(scores, np.ndarray), "Expects ndarray"
-        scores = scores >= self.threshold  # type: ignore
-        self._log_and_update('thresholded_scores', scores.tolist())
+        self._log_and_update(f'{self.compare}_similarity_scores', similarity_scores)
+        thresholded_similarity_scores = similarity_scores >= self.threshold
+        self._log_and_update(
+            'thresholded_similarity_scores', 
+            thresholded_similarity_scores
+        )
 
         # Calculate the mean reciprocal rank
         mean_reciprocal_rank = 0
-        for rank, score in enumerate(scores):
+        max_score_rank = -1
+        for rank, score in enumerate(thresholded_similarity_scores):
             if score:
+                max_score_rank = rank
                 mean_reciprocal_rank = 1/(rank+1)
+                break
 
+        if max_score_rank == -1:
+            mean_reciprocal_rank = 0.0
+
+        self._log_and_update('mean_reciprocal_rank', mean_reciprocal_rank)
         return [mean_reciprocal_rank]
 
     def _log_and_update(self, key, value):
